19144881d2d583a04f6aed618223412f4fbffcea
1735455665liu <1735455665@qq.com>
2019-04-26 13:47:31 +0800

    后台搭建

---
+++ app/Admin/Controllers/GoodsController.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+use App\Model\p_goods;
+use App\Http\Controllers\Controller;
+use Encore\Admin\Controllers\HasResourceActions;
+use Encore\Admin\Form;
+use Encore\Admin\Grid;
+use Encore\Admin\Layout\Content;
+use Encore\Admin\Show;
+
+class GoodsController extends Controller
+{
+    use HasResourceActions;
+
+    /**
+     * Index interface.
+     *
+     * @param Content $content
+     * @return Content
+     */
+    public function index(Content $content)
+    {
+        return $content
+            ->header('Index')
+            ->description('description')
+            ->body($this->grid());
+    }
+
+    /**
+     * Show interface.
+     *
+     * @param mixed $id
+     * @param Content $content
+     * @return Content
+     */
+    public function show($id, Content $content)
+    {
+        return $content
+            ->header('Detail')
+            ->description('description')
+            ->body($this->detail($id));
+    }
+
+    /**
+     * Edit interface.
+     *
+     * @param mixed $id
+     * @param Content $content
+     * @return Content
+     */
+    public function edit($id, Content $content)
+    {
+        return $content
+            ->header('Edit')
+            ->description('description')
+            ->body($this->form()->edit($id));
+    }
+
+    /**
+     * Create interface.
+     *
+     * @param Content $content
+     * @return Content
+     */
+    public function create(Content $content)
+    {
+        return $content
+            ->header('Create')
+            ->description('description')
+            ->body($this->form());
+    }
+
+    /**
+     * Make a grid builder.
+     *
+     * @return Grid
+     */
+    protected function grid()
+    {
+        $grid = new Grid(new p_goods);
+
+        $grid->id('Id');
+        $grid->name('Name');
+        $grid->price('Price');
+        $grid->store('Store');
+        $grid->is_delete('Is delete');
+
+        return $grid;
+    }
+
+    /**
+     * Make a show builder.
+     *
+     * @param mixed $id
+     * @return Show
+     */
+    protected function detail($id)
+    {
+        $show = new Show(p_goods::findOrFail($id));
+
+        $show->id('Id');
+        $show->name('Name');
+        $show->price('Price');
+        $show->store('Store');
+        $show->is_delete('Is delete');
+
+        return $show;
+    }
+
+    /**
+     * Make a form builder.
+     *
+     * @return Form
+     */
+    protected function form()
+    {
+        $form = new Form(new p_goods);
+
+        $form->text('name', 'Name');
+        $form->number('price', 'Price');
+        $form->number('store', 'Store');
+        $form->number('is_delete', 'Is delete');
+
+        return $form;
+    }
+}
---
+++ app/Admin/routes.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Routing\Router;
+
+Admin::registerAuthRoutes();
+
+Route::group([
+    'prefix'        => config('admin.route.prefix'),
+    'namespace'     => config('admin.route.namespace'),
+    'middleware'    => config('admin.route.middleware'),
+], function (Router $router) {
+
+    $router->get('/', 'HomeController@index')->name('admin.home');
+    $router->resource('/goods',GoodsController::class); //商品管理
+    $router->resource('/orders',OrdersController::class);//商品订单
+    $router->resource('/users',UserController::class);//用户管理
+//    $router->resource('/mac',OrdersController::class);//素材管理
+
+
+
+
+});
